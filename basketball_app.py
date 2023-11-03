3008697ae697913ce889b1e703be27c1d7f053fd
Kirill Borisov <66241092+kiborisov@users.noreply.github.com>
2023-11-03 12:55:14 -0400

    Update basketball_app.py

--- basketball_app.py
+++ basketball_app.py
@@ -66,9 +66,5 @@ if st.button('Intercorrelation Heatmap'):
         f, ax = plt.subplots(figsize=(7, 5))
         ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
     st.pyplot(f)
-    try:
-    corr = df.corr()
-except Exception as e:
-    st.error(f"An error occurred: {str(e)}")
 
 
